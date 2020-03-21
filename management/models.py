6fefd3426850da9daab994116426e518d3d47b49
Carlos <carlosgomes@gmail.com>
2020-03-21 12:24:59 +0000

    simplify model

--- management/models.py
+++ management/models.py
@@ -6,8 +6,12 @@ class Supplier(models.Model):
     name = models.CharField(max_length=32
                             , help_text="Nome do Fornecedor")
     email = models.EmailField(max_length=254
-                              , help_text="Contacto do Fornecedor (ie, email, telefone)"
+                              , help_text="Email do fornecedor"
                               , blank=True)
+    phone = models.CharField(max_length=32
+                              , help_text="Telefone do Fornecedor"
+                              , blank=True)
+
     lead = models.CharField(max_length=32
                             , help_text="Identificação do lead"
                             , blank=True)
@@ -16,11 +20,6 @@ class Supplier(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class Contact(models.Model):
-    supplier = models.ForeignKey(Supplier
-                                 , on_delete=models.DO_NOTHING)
 
 
 class ProductType(models.Model):
@@ -33,20 +32,34 @@ class ProductType(models.Model):
         return self.type
 
 
-class Product(models.Model):
+class ProductSubType(models.Model):
     product_type = models.ForeignKey(ProductType
                                      , on_delete=models.DO_NOTHING)
+
+    type = models.CharField(max_length=32
+                            , help_text="Tipo de Produto")
+    designation = models.CharField(max_length=128
+                                   , help_text="Designação do Produto")
+
+    def __str__(self):
+        return self.type
+
+
+class Product(models.Model):
+
+    product_subtype = models.ForeignKey(ProductSubType
+                                        , on_delete=models.DO_NOTHING)
+
     title = models.CharField(max_length=32
                              , help_text="Nome do Produto")
-    description = models.CharField(max_length=32
-                                   , help_text="Descrição do produto"
-                                   , blank=True)
+
     model = models.CharField(max_length=32
                              , help_text="Modelo do Produto"
                              , blank=True)
-    make = models.CharField(max_length=32
-                            , help_text="Marca do Produto"
-                            , blank=True)
+
+    description = models.CharField(max_length=128
+                                   , help_text="Descrição do produto"
+                                   , blank=True)
 
     def __str__(self):
         return self.title
@@ -58,16 +71,23 @@ class Proposal(models.Model):
                                  , on_delete=models.DO_NOTHING
                                  , blank=True
                                  , default=None)
+
     product = models.ForeignKey(Product
                                 , on_delete=models.DO_NOTHING)
-    availability = models.IntegerField()
-    min_order_quantity = models.IntegerField()
+
+    availability = models.IntegerField(help_text="Inserir valor total sem abreviatura")
+
+    min_order_quantity = models.IntegerField(help_text="Inserir valor total sem abreviatura")
+
     unit_price = models.DecimalField(decimal_places=2
                                      , max_digits=10)
+
     picture = models.ImageField(upload_to='./uploads/images/')
+
     payment_terms = models.CharField(max_length=128
                                      , help_text='Termos de pagamento'
                                      , blank=True)
+
     ce_certified = models.NullBooleanField()
     fda_certified = models.NullBooleanField()
 
@@ -94,15 +114,4 @@ class ProposalDocuments(models.Model):
 
     def __str__(self):
         return self.proposal.product.title
-
-
-class ProposalState(models.Model):
-    proposal = models.ForeignKey(Proposal
-                                 , on_delete=models.DO_NOTHING)
-
-
-class Donation(models.Model):
-    product = models.ForeignKey(Product
-                                , on_delete=models.DO_NOTHING)
-    availability = models.IntegerField()
 
