843239b979e41c59d515fc6ebaacef863df0e6f1
Carlos <carlosgomes@gmail.com>
2020-03-21 13:51:43 +0000

    cleanup templates

--- management/urls.py
+++ management/urls.py
@@ -4,8 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('proposal/detail/<int:pk>/', views.ProposalDetailView, name='detail'),
-    path('proposal/edit/<int:pk>', views.edit, name='edit'),
-    path('proposal/new', views.proposalview, name='proposal'),
-    path('proposal/delete/<int:pk>', views.delete, name='delete'),
+
 ]
