3f9c39cb194e838242d2765dbf847661f7e8713e
Carlos <carlosgomes@gmail.com>
2020-03-21 13:02:04 +0000

    changes

--- materialhospital/settings.py
+++ materialhospital/settings.py
@@ -11,16 +11,28 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import json
 
+from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+with open(os.path.join(BASE_DIR, 'secrets.json')) as fh:
+    secrets = json.loads(fh.read())
+
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "{0} key missing".format(setting)
+        raise ImproperlyConfigured(error_msg)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f-oaz_-=btr7sepemdwhgn32b9mn2h7jmd2x7#^mo_0^t5+k^*'
+SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
